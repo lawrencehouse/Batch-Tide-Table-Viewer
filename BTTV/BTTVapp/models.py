deb08a857ae5eaf441ff7b06109355b85a799ab2
Lawrence <trehouse650@gmail.com>
2020-07-05 19:58:35 -0800

    Aligned logo, fixed up CSS

--- BTTV/BTTVapp/models.py
+++ BTTV/BTTVapp/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class States(models.Model):
+class Regions(models.Model):
+    region_name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.region_name
+
+class WestUS(models.Model):
     state_name = models.CharField(max_length=200)
 
+class EastUS(models.Model):
+    state_name = models.CharField(max_length=200)
